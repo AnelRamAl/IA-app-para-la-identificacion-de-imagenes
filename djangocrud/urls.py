ed59ee2206286f3e3b9a4bd7963ed9c6c773bfa5
Anel <anel.ramirez.al@gmail.com>
2023-12-05 16:04:45 -0600

    initial commit

---
+++ djangocrud/urls.py
@@ -0,0 +1,45 @@
+"""
+URL configuration for djangocrud project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from training import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name = 'home'),
+    path('signup/', views.signup, name = 'signup'),
+    path('training/', views.trainingf, name = 'training'),
+    path('logout/', views.signout, name = 'logout'),
+    path('signin/', views.signin, name = 'signin'),    
+    path('guardar_training/', views.guardar_training, name = 'guardar_training'),
+    path('model_knowledge/', views.model_knowledge, name = 'model_knowledge'),
+    # path('json_file/', views.json_file, name = 'json_file'),
+    path('json_file_T/', views.json_file_T, name = 'json_file_T')
+
+]
+
+
+# ...
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
